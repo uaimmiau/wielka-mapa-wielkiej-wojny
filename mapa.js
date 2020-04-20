cdbc8978a6f54a9cdd257bd5170a7164bafeeecf
uaimmiau <62911081+uaimmiau@users.noreply.github.com>
2020-04-20 14:41:40 +0200

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,17 +1,17 @@
-const h0907 = document.getElementById('h0907');
-const h0908 = document.getElementById('h0908');
-const h1008 = document.getElementById('h1008');
-const h0909 = document.getElementById('h0909');
-const h1729 = document.getElementById('h1729');
-const h1829 = document.getElementById('h1829');
-const h1830 = document.getElementById('h1830');
+// const h0907 = document.getElementById('h0907');
+// const h0908 = document.getElementById('h0908');
+// const h1008 = document.getElementById('h1008');
+// const h0909 = document.getElementById('h0909');
+// const h1729 = document.getElementById('h1729');
+// const h1829 = document.getElementById('h1829');
+// const h1830 = document.getElementById('h1830');
 
 
-//Mosty dla pana starosty
-h0907.classList.add('most_l');
-h0909.classList.add('most_r');
-h1830.classList.add('most_r');
-h0908.classList.add('most_t');
-h1729.classList.add('most_t');
-h1008.classList.add('most_b');
-h1829.classList.add('most_b');
+// //Mosty dla pana starosty
+// h0907.classList.add('most_l');
+// h0909.classList.add('most_r');
+// h1830.classList.add('most_r');
+// h0908.classList.add('most_t');
+// h1729.classList.add('most_t');
+// h1008.classList.add('most_b');
+// h1829.classList.add('most_b');
